afdacbce729b7c5a4852ad8d633f517b180af9a9
Pedro Paulo <0901dantaspedro@gmail.com>
2025-04-08 01:25:22 -0300

    add: Apontar a rodada de cada temporada com o maior número de gols marcados

---
+++ projeto-dados-brasileirao/Analises_Partidas/14_Rodada_com_mais_gols_por_temporada.sql
@@ -0,0 +1,41 @@
+/* Apontar a rodada de cada temporada com o maior número de gols marcados. */
+
+with mais_gols_rodada as (
+
+    select
+        case 
+            when date(substr(t1.Data, 7, 4) || '-' || substr(t1.Data, 4, 2) || '-' || substr(t1.Data, 1, 2)) 
+                between '2020-08-08' and '2021-02-25' then '2020'
+            else substr(t1.Data, 7, 4)
+        end as Temporada,
+        rodata as rodada,
+        mandante,
+        visitante,
+        (mandante_Placar + visitante_Placar) as gols_partida
+
+    from tb_brasileirao as t1
+
+),
+
+ranking as (
+
+    select
+        *,
+        rank() over (partition by Temporada order by gols_partida desc) as posicao
+    
+    from mais_gols_rodada
+
+)
+
+select
+    Temporada,
+    rodada,
+    mandante,
+    visitante,
+    gols_partida
+
+from ranking
+
+where posicao = 1
+
+order by Temporada asc, gols_partida desc
