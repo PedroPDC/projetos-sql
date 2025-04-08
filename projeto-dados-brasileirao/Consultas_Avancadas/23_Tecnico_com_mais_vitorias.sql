030d1e1f7a8ccd48211005b7d05239e0e8cf17fd
Pedro Paulo <0901dantaspedro@gmail.com>
2025-04-08 01:40:17 -0300

    add: Identificar o técnico com mais vitórias no período analisado

---
+++ projeto-dados-brasileirao/Consultas_Avancadas/23_Tecnico_com_mais_vitorias.sql
@@ -0,0 +1,20 @@
+/* Identificar o técnico com mais vitórias no período analisado. */
+
+select
+
+    case
+        when mandante = vencedor then tecnico_mandante
+        when visitante = vencedor then tecnico_visitante
+        else '-'
+    end as tecnico_vencedor,
+
+    count(*) as vitorias
+
+from tb_brasileirao
+
+where vencedor <> '-'
+and tecnico_vencedor is not null
+
+group by tecnico_vencedor
+
+order by vitorias desc
