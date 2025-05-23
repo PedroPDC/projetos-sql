91be76279f96e92543b91e372e401c8550420be4
Pedro Paulo <0901dantaspedro@gmail.com>
2025-05-23 20:02:11 -0300

    add: Pilotos mais jovens a serem campeoes mundiais de F1

---
+++ projeto-formula-one/Analises_Pilotos/43_pilotos_campeoes_mundiais_mais_jovens.sql
@@ -0,0 +1,67 @@
+/*
+Idade dos pilotos mais jovens a serem campeoes mundiais
+*/
+
+with pontos_por_piloto as (
+
+    select
+        t1.driverId,
+        t2.year,
+        sum(t1.points) as total_pontos
+    
+    from tb_results as t1
+
+    left join tb_races as t2
+    on t1.raceId = t2.raceId
+
+    group by t1.driverId, t2.year
+
+),
+
+campeoes_por_ano as (
+
+    select
+        t1.driverId,
+        t1.year,
+        t2.forename,
+        t2.surname,
+        t2.dob,
+
+        rank() over (
+            partition by t1.year
+            order by t1.total_pontos desc
+        
+        ) as posicao
+    
+    from pontos_por_piloto as t1
+
+    join tb_drivers as t2
+    on t1.driverId = t2.driverId
+
+),
+
+apenas_campeoes as (
+
+    select
+        driverId,
+        year,
+        concat(forename, ' ', surname) as Pilotos,
+        dob
+    
+    from campeoes_por_ano
+
+    where posicao = 1
+
+)
+
+select
+    Pilotos,
+    year as Temporada,
+    dob as Data_de_Nascimento,
+    (year - strftime('%Y', dob)) as Idade
+
+from apenas_campeoes
+
+order by Idade asc
+
+limit 10
